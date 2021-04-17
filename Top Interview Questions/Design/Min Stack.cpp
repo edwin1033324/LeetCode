bcd3efdd29e6657e2343a05abd31ca56dbab8b2a
edwin1033324 <71120625+edwin1033324@users.noreply.github.com>
2021-04-17 13:10:03 +0800

    Create Min Stack.cpp

---
+++ Top Interview Questions/Design/Min Stack.cpp
@@ -0,0 +1,41 @@
+// Min Stack
+
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    MinStack() {
+        min_val = INT_MAX;
+    }  
+    void push(int x) {
+        if (x <= min_val) {
+            st.push(min_val);
+            min_val = x;
+        }
+        st.push(x);
+    }   
+    void pop() {
+        int t = st.top(); st.pop();
+        if (t == min_val) {
+            min_val = st.top(); st.pop();
+        }
+    }  
+    int top() {
+        return st.top();
+    }    
+    int getMin() {
+        return min_val;
+    }
+
+private:
+    int min_val;
+    stack<int> st;
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
