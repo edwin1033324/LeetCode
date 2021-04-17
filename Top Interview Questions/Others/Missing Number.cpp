e8c2a66d6400afb10e5c5c844dd0c8985c7b477b
edwin1033324 <71120625+edwin1033324@users.noreply.github.com>
2021-04-17 13:18:38 +0800

    Create Missing Number.cpp

---
+++ Top Interview Questions/Others/Missing Number.cpp
@@ -0,0 +1,18 @@
+// Missing Number
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int left = 0, right = nums.size();
+        while (left < right) 
+        {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > mid) 
+                right = mid;
+            else 
+                left = mid + 1;
+        }
+        return right;
+    }
+};
