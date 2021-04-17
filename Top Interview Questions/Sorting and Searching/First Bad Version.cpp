5d900e35e5655d9e976931a6b4c334c9c5a79a22
edwin1033324 <71120625+edwin1033324@users.noreply.github.com>
2021-04-17 13:00:43 +0800

    Create First Bad Version.cpp

---
+++ Top Interview Questions/Sorting and Searching/First Bad Version.cpp
@@ -0,0 +1,20 @@
+// First Bad Version
+
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left = 0, right = n - 1;
+        while (left < right) 
+        {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid + 1)) 
+                right = mid;
+            else 
+                left = mid + 1;
+        }
+        return right + 1;
+    }
+};
